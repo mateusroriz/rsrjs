4e0f25b0c2aa15fc2c459ab31a523df4201cf49e
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-08-28 23:52:59 -0400

    storing states

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,17 +2,27 @@ import React, {Component} from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component{
+    state = { //eh um objeto e conseguimos armazenar variaveis aqui
+        products:[],
+    }
+
 componentDidMount(){ //executar uma ação assim que o componente for exibito em tela
         this.loadProducts();
 }
 loadProducts = async () => {
     const response = await api.get('/products');
 
-    console.log(response.data.docs);
+    this.setState({products: response.data.docs})
 };
 
 render(){
-    return <h1>Hello Programmer</h1>
+   return(
+       <div className ='product-list'>
+           {this.state.products.map(product=> (
+               <h2 key ={product._id}>{product.title}</h2>
+           ))}
+        </div>
+   )
     }
 
 }
